3edb1c2aa3824338934e83295e3ecc13d4c3b040
Nicolas Hardmeier <nicolas.hardmeier@yahoo.es>
2019-09-18 14:20:13 +0200

    Add files via upload

---
+++ precache-manifest.090978ac6d27d83219f123ca01fc294b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/lyrics_gen/robots.txt"
+  },
+  {
+    "revision": "defc5804e7068c1c9ac4",
+    "url": "/lyrics_gen/js/chunk-vendors.87cdf293.js"
+  },
+  {
+    "revision": "4cdf19beab178775fe15",
+    "url": "/lyrics_gen/js/app.a3505d08.js"
+  },
+  {
+    "revision": "289ec347741b85f36976",
+    "url": "/lyrics_gen/js/about.f8f491e2.js"
+  },
+  {
+    "revision": "dc97aa5b4b12c9f8d8b87cf2c1fa59c7",
+    "url": "/lyrics_gen/index.html"
+  },
+  {
+    "revision": "defc5804e7068c1c9ac4",
+    "url": "/lyrics_gen/css/chunk-vendors.5b99be6e.css"
+  },
+  {
+    "revision": "4cdf19beab178775fe15",
+    "url": "/lyrics_gen/css/app.d833abdd.css"
+  }
+];
